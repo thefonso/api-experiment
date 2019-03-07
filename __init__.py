bb06881e725abedfae6be41b4e645667d169f48a
thefonso <thefonso@thefonso.com>
2019-03-07 10:42:51 -0500

    WIP: working on mergeing

--- __init__.py
+++ __init__.py
@@ -1,16 +1,27 @@
 import random
 from flask import Flask, render_template
 from starwars import (get_films, get_planet, get_planets, get_ship, get_people)
+from collections import defaultdict
 
 app = Flask(__name__)
+
+l1 = [{"index":1, "b":2}, {"index":2, "b":3}, {"index":3, "green":"eggs"}]
+l2 = [{"index":1, "c":4}, {"index":2, "c":5}]
+
+d = defaultdict(dict)
+for l in (l1, l2):
+    for elem in l:
+        d[elem['index']].update(elem)
+l3 = d.values()
+
 
 @app.route('/')
 def index():
     context = {
-        "films": get_films()['count'],
         "planets": get_planets()['results'],
         "people": get_people()['results'],
         "both": get_planets()['results'] + get_people()['results'],
+        "test": l3,
     }
     return render_template("index.html", **context, )
 
